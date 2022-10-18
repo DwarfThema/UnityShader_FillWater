3d3ef1589f5b5eeeedd2a0c5fd8b35cd36692fbe
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-10-19 00:00:31 +0900

    채우면 커지는 오브제

---
+++ WaterFall.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class WaterFall : UdonSharpBehaviour
+{
+    ParticleSystem myParticleSystem;
+
+    void Start()
+    {
+        myParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Angle(Vector3.down, transform.forward) <= 90f)
+        {
+            myParticleSystem.Play();
+        }
+        else
+        {
+            myParticleSystem.Stop();
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.gameObject.name == "water")
+        {
+            other.gameObject.transform.localScale += new Vector3(0, 0.001f, 0);
+        }
+    }
+}
